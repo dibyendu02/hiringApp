4262122a6b295b41444d9d4f5f59ef54461ee118
Dibyendu Das <get2dibyendu@gmail.com>
2024-04-11 19:09:20 +0530

    contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const About = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Services = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
